1ef8c437016dc4a6adc1ce9b8466a9ab64f442c3
Lamont Granquist <lamont@scriptkiddie.org>
2017-09-19 13:22:09 -0700

    deep merge on the fly
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef/node/immutable_collections.rb
+++ lib/chef/node/immutable_collections.rb
@@ -30,14 +30,12 @@ class Chef
         e
       end
 
-      def convert_value(value)
+      def convert_value(value, path = nil)
         case value
         when Hash
-          ImmutableMash.new(value, __root__, __node__, __precedence__)
+          ImmutableMash.new({}, __root__, __node__, __precedence__, path)
         when Array
-          ImmutableArray.new(value, __root__, __node__, __precedence__)
-        when ImmutableMash, ImmutableArray
-          value
+          ImmutableArray.new([], __root__, __node__, __precedence__, path)
         else
           safe_dup(value).freeze
         end
@@ -60,6 +58,15 @@ class Chef
     #   detect staleness and raise an error if accessed when stale.
     class ImmutableArray < Array
       include Immutablize
+
+      methods = Array.instance_methods - Object.instance_methods +
+        [ :!, :!=, :<=>, :==, :===, :eql?, :to_s, :hash, :key, :has_key?, :inspect, :pretty_print, :pretty_print_inspect, :pretty_print_cycle, :pretty_print_instance_variables ]
+
+      methods.each do |method|
+        define_method method do |*args, &block|
+          merged_array.public_send(method, *args, &block)
+        end
+      end
 
       alias :internal_push :<<
       private :internal_push
@@ -96,7 +103,45 @@ class Chef
 
       alias_method :to_array, :to_a
 
+      def [](key)
+        merged_lazy_array[key]
+      end
+
       private
+
+      def combined_components(components)
+        combined_values = nil
+        components.each do |component|
+          values = __node__.attributes.instance_variable_get(component).read(*__path__)
+          next unless values.is_a?(Array)
+          combined_values ||= []
+          combined_values += values
+        end
+        combined_values
+      end
+
+      def get_array(component)
+        array = __node__.attributes.instance_variable_get(component).read(*__path__)
+        if array.is_a?(Array)
+          array
+        end # else nil
+      end
+
+      def merged_lazy_array
+        components = []
+        components << combined_components(Attribute::DEFAULT_COMPONENTS)
+        components << get_array(:@normal)
+        components << combined_components(Attribute::OVERRIDE_COMPONENTS)
+        components << get_array(:@automatic)
+        highest = components.compact.last
+        if highest.is_a?(Array)
+          highest.map { |x| convert_value(x) }
+        end # else nil
+      end
+
+      def merged_array
+        merged_lazy_array.to_a
+      end
 
       # needed for __path__
       def convert_key(key)
@@ -123,16 +168,28 @@ class Chef
       include Immutablize
       include CommonAPI
 
+      methods = Hash.instance_methods - Object.instance_methods +
+        [ :!, :!=, :<=>, :==, :===, :eql?, :to_s, :hash, :key, :has_key?, :inspect, :pretty_print, :pretty_print_inspect, :pretty_print_cycle, :pretty_print_instance_variables ]
+
+      methods.each do |method|
+        define_method method do |*args, &block|
+          #raise "foo" if method == :each_pair
+          #puts "METHOD: #{method}"
+          #super(*args, &block)
+          merged_lazy_hash.public_send(method, *args, &block)
+        end
+      end
+
       # this is for deep_merge usage, chef users must never touch this API
       # @api private
       def internal_set(key, value)
-        regular_writer(key, convert_value(value))
+        regular_writer(key, convert_value(value, __path__ + [ key ]))
       end
 
       def initialize(mash_data = {})
-        mash_data.each do |key, value|
-          internal_set(key, value)
-        end
+        #mash_data.each do |key, value|
+        #  internal_set(key, value)
+        #end
       end
 
       alias :attribute? :has_key?
@@ -166,14 +223,44 @@ class Chef
         h
       end
 
+      def [](key)
+        Attribute::COMPONENTS.reverse.each do |component|
+          value = __node__.attributes.instance_variable_get(component).read(*__path__, key)
+          unless value.nil?
+            return convert_value(value, __path__ + [ key ])
+          end
+        end
+        nil
+      end
+
       alias_method :to_hash, :to_h
 
-      # For elements like Fixnums, true, nil...
-      def safe_dup(e)
-        e.dup
-      rescue TypeError
-        e
+      private
+
+      def merged_lazy_hash
+        hash = {}
+        Attribute::COMPONENTS.reverse.each do |component|
+          subhash = __node__.attributes.instance_variable_get(component).read(*__path__)
+          unless subhash.nil? # FIXME: nil is used for not present
+            if subhash.kind_of?(Hash)
+              subhash.keys.each do |key|
+                next if hash.key?(key)
+                #hash[key] = subhash[key]
+                hash[key] = convert_value(subhash[key], __path__ + [ key ])
+                #puts "BOOM: #{hash[key].__path__}"
+                #puts hash[key].class
+              end
+            else
+              return hash
+            end
+          end
+        end
+        hash
       end
+
+      #def merged_hash
+      #  merged_lazy_hash.to_h
+      #end
 
       prepend Chef::Node::Mixin::StateTracking
       prepend Chef::Node::Mixin::ImmutablizeHash
